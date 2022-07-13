b932408c988eb3b763bdef51073a125b5b9a57fb
Nina Okumura <elaineokumura@gmail.com>
2022-07-13 21:29:57 +1200

    creating layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,14 @@
-import logo from "./logo.svg";
-import "./App.css";
+import PriceCard from "./components/PriceCard";
+import { ReactComponent as CirclesPattern } from "./assets/images/pattern-circles.svg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-light-grayish-slider h-screen border border-red w-full grid place-content-center relative font-sans p-4">
+      <div className="z-10">
+        <div className="absolute h-[50vh] top-0 left-0 right-0 bg-very-pale-blue rounded-bl-[10vw]" />
+        <CirclesPattern className=" absolute top-8 right-0 left-0 m-auto" />
+      </div>
+      <PriceCard />
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        circles: "url('./assets/images/pattern-circles.svg')",
+      },
+      fontFamily: {
+        sans: ["Manrope", "sans-serif"],
+      },
+      colors: {
+        "soft-cyan": "hsl(174, 77%, 80%)",
+        "strong-cyan": "hsl(174, 86%, 45%)",
+        "light-grayish-red": "hsl(14, 92%, 95%)",
+        "light-red": "hsl(15, 100%, 70%)",
+        "pale-blue": "hsl(226, 100%, 87%)",
+        "price-white": "hsl(0, 0%, 100%)",
+        "very-pale-blue": "hsl(230, 100%, 99%)",
+        "light-grayish-slider": "hsl(224, 65%, 95%)",
+        "light-grayish-toggle": "hsl(223, 50%, 87%)",
+        "grayish-blue": "hsl(225, 20%, 60%)",
+        "dark-desaturated-blue": "hsl(227, 35%, 25%)",
+      },
+    },
   },
   plugins: [],
 };
