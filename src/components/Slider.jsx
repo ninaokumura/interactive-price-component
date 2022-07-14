4c28367c8e22f24c31de70a22ca710b0bfde966b
Nina Okumura <elaineokumura@gmail.com>
2022-07-14 21:58:53 +1200

    fix hover state

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 
-export default function Slider(props) {
+export default function Slider({
+  color1 = "hsl(174, 77%, 80%)",
+  color2 = "hsl(224, 65%, 95%)",
+  ...props
+}) {
   const value = Number(props.value);
   const min = Number(props.min);
   const max = Number(props.max);
@@ -16,9 +20,9 @@ export default function Slider(props) {
         {...props}
         className="form-range appearance-none rounded-full h-2 slider-thumb"
         style={{
-          backgroundImage: `linear-gradient(to right, hsl(174, 77%, 80%) 0%,hsl(174, 77%, 80%) ${fillRatio}%, hsl(224, 65%, 95%) ${
+          backgroundImage: `linear-gradient(to right, ${color1} 0%,${color1} ${fillRatio}%, ${color2} ${
             fillRatio + 1
-          }%, hsl(224, 65%, 95%) 100%)`,
+          }%, ${color2} 100%)`,
         }}
       />
     </>
